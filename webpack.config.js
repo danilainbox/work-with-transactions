ce51d11ff64d9b0ab2945292cb06e0e2b41bf7b1
dscherbak <danila-inbox@yandex.ru>
2017-07-22 15:29:20 +0300

    package.json, server.js, webpack.config.js added

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+var webpack = require('webpack'),
+    ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+
+module.exports = {
+    entry: './app/app.js',
+
+    output: {
+        path: 'public',
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+
+    plugins: process.env.NODE_ENV === 'production' ? [
+            new webpack.optimize.DedupePlugin(),
+            new webpack.optimize.OccurrenceOrderPlugin(),
+            new webpack.optimize.UglifyJsPlugin(),
+            new webpack.ProvidePlugin({
+                '$': 'jquery',
+                'jQuery': 'jquery',
+                'bootstrap': 'bootstrap'
+            }),
+            new ExtractTextPlugin('bundle.css')
+        ] : [
+            new ExtractTextPlugin('bundle.css'),
+            new webpack.ProvidePlugin({
+                '$': 'jquery',
+                'jQuery': 'jquery',
+                'bootstrap': 'bootstrap'
+            })
+        ],
+
+    module: {
+        loaders: [
+            { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' },
+            {
+                test: /\.less$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader!less-loader')
+            },
+            { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff&name=fonts/[name].[ext]" },
+            { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader?name=fonts/[name].[ext]" }
+        ]
+    }
+};
